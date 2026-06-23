e7e80c3d87f0ae059b34496fac3a0ed4713034b6
Babs <bwanyeki12@gmail.com>
2026-06-23 00:21:26 -0400

    iOS app

---
+++ ios/RestaurantCompass/RestaurantCompassTests/RestaurantCompassTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantCompassTests.swift
+//  RestaurantCompassTests
+//
+//  Created by Babu Wanyeki on 6/22/26.
+//
+
+import Testing
+@testable import RestaurantCompass
+
+struct RestaurantCompassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
